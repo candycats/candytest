608647bc49a7a53904d83d564b819b025bf65802
candycats <523842194@qq.com>
2018-04-28 17:09:38 +0800

    add webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,24 @@
-testtesttest
+const path = require('path');
+
+module.exports = {
+  // TS 执行入口文件
+  entry: './main',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  resolve: {
+    // 先尝试 ts，tsx 后缀的 TypeScript 源码文件 
+    extensions: ['.ts', '.tsx', '.js',] 
+  },
+  module: {
+    rules: [
+      {
+        // 同时匹配 ts，tsx 后缀的 TypeScript 源码文件 
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  },
+  devtool: 'source-map',// 输出 Source Map 方便在浏览器里调试 TypeScript 代码
+};
